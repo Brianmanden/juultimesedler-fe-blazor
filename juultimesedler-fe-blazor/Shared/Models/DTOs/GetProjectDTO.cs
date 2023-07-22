64d5a0e2f286b6fc00d5f2a09505963a58b3088c
Brian Andersen <brianmanden@gmail.com>
2023-07-22 16:51:42 +0200

    MOVE POC to index.razor ADD WIP Fetch projects from BE API

---
+++ juultimesedler-fe-blazor/Shared/Models/DTOs/GetProjectDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace juultimesedler_fe_blazor.Shared.Models.DTOs
+{
+    public class GetProjectDTO
+    {
+        public int ProjectId { get; set; }
+        public string? ProjectName { get; set; }
+        public string? ProjectFullName { get; set; }
+    }
+}

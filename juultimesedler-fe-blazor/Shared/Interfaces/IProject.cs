52114b038356c3545856b05188f73a2b999dbefd
Brian Andersen <brianmanden@gmail.com>
2023-07-24 19:26:55 +0200

    MOVE GetProjects into service ADD interface CLEAN UP Code  ADD regions and comments

---
+++ juultimesedler-fe-blazor/Shared/Interfaces/IProject.cs
@@ -0,0 +1,8 @@
+﻿using juultimesedler_fe_blazor.Shared.Models.DTOs;
+
+namespace juultimesedler_fe_blazor.Shared;
+
+public interface IProjectInterface
+{
+    Task<IEnumerable<GetProjectDTO>> GetProjects(HttpClient httpClient, string uri);
+}

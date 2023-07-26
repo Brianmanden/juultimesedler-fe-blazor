a20a63f984d142e1c0022e1e09d6a9d641fa589f
Brian Andersen <brianmanden@gmail.com>
2023-07-26 22:34:43 +0200

    WIP ADD DTOs

--- juultimesedler-fe-blazor/Shared/Models/DTOs/GetProjectDTO.cs
+++ juultimesedler-fe-blazor/Shared/Models/DTOs/GetProjectDTO.cs
@@ -1,9 +1,8 @@
-﻿namespace juultimesedler_fe_blazor.Shared.Models.DTOs
+﻿namespace juultimesedler_fe_blazor.Shared.Models.DTOs;
+
+public class GetProjectDTO
 {
-    public class GetProjectDTO
-    {
-        public int ProjectId { get; set; }
-        public string? ProjectName { get; set; }
-        public string? ProjectFullName { get; set; }
-    }
+    public int ProjectId { get; set; }
+    public string? ProjectName { get; set; }
+    public string? ProjectFullName { get; set; }
 }
---
+++ juultimesedler-fe-blazor/Shared/Models/DTOs/TasksGroupDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace juultimesedler_fe_blazor.Shared.Models.DTOs;
+
+public class TasksGroupDTO
+{
+    public string TaskGroupName { get; set; }
+    public List<string> TaskNames { get; set; }// = new List<string>();
+}

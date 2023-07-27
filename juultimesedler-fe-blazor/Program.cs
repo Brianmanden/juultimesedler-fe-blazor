3543ca4f99ee0dc7bf971e3038c4d25c80b46fb9
Brian Andersen <brianmanden@gmail.com>
2023-07-27 15:30:16 +0200

    REFACTOR to use viewmodel

--- juultimesedler-fe-blazor/Program.cs
+++ juultimesedler-fe-blazor/Program.cs
@@ -1,5 +1,6 @@
 using juultimesedler_fe_blazor;
 using juultimesedler_fe_blazor.Services;
+using juultimesedler_fe_blazor.ViewModels;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ProjectsService>();
 builder.Services.AddScoped<TasksService>();
+builder.Services.AddScoped<IndexViewModel>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ juultimesedler-fe-blazor/ViewModels/IndexViewModel.cs
@@ -0,0 +1,56 @@
+﻿using juultimesedler_fe_blazor.Models;
+using juultimesedler_fe_blazor.Services;
+using juultimesedler_fe_blazor.Shared.Models.DTOs;
+
+namespace juultimesedler_fe_blazor.ViewModels;
+
+public class IndexViewModel
+{
+    private ProjectsService _projectsService;
+    private TasksService _tasksService;
+    private GetProjectDTO[]? _projects;
+    private TasksGroupDTO[]? _groupedTasks;
+
+    public GetProjectDTO SelectedProject { get; set; }
+    public string? TasksSearchText { get; set; }
+    public TimeSpan? StartingTime { get; set; } = new TimeSpan(02, 35, 00);
+    public TimeSpan? EndingTime { get; set; } = new TimeSpan(02, 35, 00);
+
+    public IndexViewModel(ProjectsService projectService, TasksService tasksService)
+    {
+        _projectsService = projectService;
+        _tasksService = tasksService;
+    }
+
+    public void SetPredefinedMinutes(TimeType whichTime, int minutes)
+    {
+        if (whichTime == TimeType.EndingTime)
+        {
+            EndingTime = TimeSpan.Parse(EndingTime!.Value.Hours + ":" + minutes + ":00");
+        }
+        else
+        {
+            StartingTime = TimeSpan.Parse(StartingTime!.Value.Hours + ":" + minutes + ":00");
+        }
+        Console.WriteLine(EndingTime);
+    }
+
+    public async Task ClearProject() { SelectedProject = null; }
+
+    public async Task<IEnumerable<GetProjectDTO>> ChooseProject(string project)
+    {
+        if (string.IsNullOrEmpty(project))
+        {
+            return _projects;
+        }
+        return _projects.Where(p => p.ProjectName.Contains(project, StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    public async Task Initialize()
+    {
+        _projects = await _projectsService.GetProjects();
+        _groupedTasks = await _tasksService.GetTasks();
+    }
+
+    //TODO BJA Create method to send form to BE
+}

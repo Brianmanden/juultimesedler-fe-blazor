8975bdd1a08f51c6a99ae918d2b52a30b1db4d37
Brian Andersen <brianmanden@gmail.com>
2023-08-03 16:25:39 +0200

    ADD Model and DTO

---
+++ juultimesedler-fe-blazor/Shared/Models/DTOs/PutTimesheetDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace juultimesedler_fe_blazor.Shared.Models.DTOs;
+
+public class PutTimesheetDTO
+{
+    public int WorkerId { get; set; }
+    public int WeekNumber { get; set; }
+    public List<WorkDay>? Workdays { get; set; }
+}
---
+++ juultimesedler-fe-blazor/Shared/Models/WorkDay.cs
@@ -0,0 +1,11 @@
+﻿namespace juultimesedler_fe_blazor.Shared.Models;
+
+public class WorkDay
+{
+    public int WeekDay { get; set; }
+    public int SelectedProjectId { get; set; }
+    public string[] SelectedTasks { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+    public string TaskComments { get; set; }
+}
